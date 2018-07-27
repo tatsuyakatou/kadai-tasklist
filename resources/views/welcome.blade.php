7c90e754fc517763b8e1f20044a667864f0fa118
EC2 Default User <ec2-user@ip-172-31-25-6.us-east-2.compute.internal>
2018-07-27 05:30:33 +0000

    CRUD task

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel6
                 </div>
 
                 <div class="links">
