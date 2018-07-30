2ce778ad070abf9c410f34234bb7779f87414ab7
EC2 Default User <ec2-user@ip-172-31-25-6.us-east-2.compute.internal>
2018-07-30 09:42:15 +0000

    bootstrap design

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,21 +3,25 @@
 @section('content')
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+    
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">    
 
-    <table class="table table-bordered">
-        <tr>
-            <th>id</th>
-            <td>{{ $task->id }}</td>
-        </tr>
-        <tr>
-            <th>ステータス</th>
-            <td>{{ $task->status }}</td>
-        </tr>
-        <tr>
-            <th>タスク内容</th>
-            <td>{{ $task->content }}</td>
-        </tr>
-    </table>
+
+            <table class="table table-bordered">
+            <tr>
+                <th>id</th>
+                <td>{{ $task->id }}</td>
+            </tr>
+            <tr>
+                <th>ステータス</th>
+                <td>{{ $task->status }}</td>
+            </tr>
+            <tr>
+                <th>タスク内容</th>
+                <td>{{ $task->content }}</td>
+            </tr>
+        </table>
 
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
@@ -25,5 +29,8 @@
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
+
+        </div>
+    </div>
 
 @endsection
